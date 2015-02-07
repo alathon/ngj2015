6f1cc4e15ee63f373c6141bc56b42e7ae5e2031d
Till Riemer <till.riemer@gmail.com>
2015-02-07 19:25:58 +0100

    got camera working again, fixed controls a bit

--- Assets/Scripts/GamepadController.cs
+++ Assets/Scripts/GamepadController.cs
@@ -34,20 +34,20 @@ public class GamepadController : MonoBehaviour
 	    Vector2 armMovement = GamePad.GetAxis(_arm, _index);
         _limbController.AddForce(isLeft ? LimbController.Limb.LeftHand : LimbController.Limb.RightHand,
             isLeft ? new Vector3(-armMovement.y * armLeftRightFactor,
-                                armUpForce,
+                                armMovement != Vector2.zero ? armUpForce : 0,
                                 -armMovement.x * armForwardBackwardFactor)
                     : new Vector3(armMovement.y * armLeftRightFactor,
-                                armUpForce,
+                                armMovement != Vector2.zero ? armUpForce : 0,
                                 armMovement.x * armForwardBackwardFactor));
 
 	    // leg 2 axes
 	    Vector2 legMovement = GamePad.GetAxis(_leg, _index);
         _limbController.AddForce(isLeft ? LimbController.Limb.LeftFoot : LimbController.Limb.RightFoot,
             isLeft ? new Vector3(-legMovement.y * legLeftRightFactor,
-                                legUpForce,
+                                legMovement != Vector2.zero ? legUpForce : 0,
                                 -legMovement.x * legForwardBackwardFactor)
                     : new Vector3(legMovement.y * legLeftRightFactor,
-                                legUpForce,
+                                legMovement != Vector2.zero ? legUpForce : 0,
                                 legMovement.x * legForwardBackwardFactor));
 
 	    // release button
