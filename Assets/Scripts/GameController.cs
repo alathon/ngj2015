d8ba61ef701c5efcc950d0edbaac1837d8900dc0
Till Riemer <till.riemer@gmail.com>
2015-02-07 13:58:22 +0100

    fixed unityvs issue added automatic camera switch if oculus not connected

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject normalCam;
+    public GameObject oculusBase;
+
+	void Start ()
+	{
+	    OVRManager.TrackingAcquired += OnOculusStart;
+        OVRManager.TrackingLost += OnOculusEnd;
+	    if (OVRManager.capiHmd != null)
+	    {
+	        OVRManager.DismissHSWDisplay(); // disable safety message
+            OnOculusStart();
+	    }
+	    else
+	    {
+	        OnOculusEnd();
+	    }
+	}
+
+    void OnDestroy()
+    {
+        OVRManager.TrackingAcquired -= OnOculusStart;
+        OVRManager.TrackingLost -= OnOculusEnd;
+    }
+
+    private void OnOculusStart()
+    {
+        normalCam.SetActive(false);
+        oculusBase.SetActive(true);
+    }
+
+    private void OnOculusEnd()
+    {
+        oculusBase.SetActive(false);
+        normalCam.SetActive(true);
+    }
+}
