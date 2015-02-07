4280fa906575e582c84c0d088d422ff3af59e1dc
alathon <martin@itsolveonline.net>
2015-02-07 17:41:51 +0100

    Changed Collectible.

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,24 +4,29 @@ using System.Collections;
 public class Collectible : MonoBehaviour {
 	private GameObject stuck;
 	private Transform pastParent;
+    private BoxCollider collider;
+
+    void Awake()
+    {
+        this.collider = this.GetComponent<BoxCollider>();
+    }
 
 	public bool IsStuck() {
 		return stuck != null;
 	}
 
 	public void StickTo(GameObject gObj) {
-		this.GetComponent<BoxCollider> ().isTrigger = true;
+		this.collider.enabled = false;
 		this.stuck = gObj;
 		this.pastParent = this.transform.parent;
 		this.transform.SetParent (gObj.transform);
-
 	}
 
 	public void Unstick() {
-		this.stuck = null;
-		this.GetComponent<BoxCollider> ().isTrigger = false;
 		if (this.pastParent != null) {
 			this.transform.SetParent(this.pastParent);
 		}
+        this.stuck = null;
+        this.collider.enabled = true;
 	}
 }
