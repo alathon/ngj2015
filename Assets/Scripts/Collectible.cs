4ecc4a623f1bd96fee7e115015fdab4114599078
alathon <martin@itsolveonline.net>
2015-02-07 12:54:10 +0100

    Last nights stuff.

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour {
+	private GameObject stuck;
+	private Transform pastParent;
+
+	public bool IsStuck() {
+		return stuck != null;
+	}
+
+	public void StickTo(GameObject gObj) {
+		this.GetComponent<BoxCollider> ().isTrigger = true;
+		this.stuck = gObj;
+		this.pastParent = this.transform.parent;
+		this.transform.SetParent (gObj.transform);
+
+	}
+
+	public void Unstick() {
+		this.stuck = null;
+		this.GetComponent<BoxCollider> ().isTrigger = false;
+		if (this.pastParent != null) {
+			this.transform.SetParent(this.pastParent);
+		}
+
+		this.rigidbody.AddExplosionForce (10, this.transform.position, 1f);
+	}
+}
---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabber : MonoBehaviour {
+	[SerializeField]
+	public GameObject test;
+
+	public Collectible collectible;
+
+	public void OnCollisionEnter(Collision col) {
+		Collectible coll = col.gameObject.GetComponent<Collectible> ();
+		if (coll) {
+			this.Collect(coll);
+		}
+	}
+
+	private void Collect(Collectible coll) {
+		coll.StickTo (this.gameObject);
+		this.collectible = coll;
+	}
+
+	public void Drop() {
+		if (this.collectible != null) {
+			this.collectible.Unstick ();
+			this.rigidbody.AddExplosionForce (10, this.transform.position, 1f);
+			this.collectible = null;
+		}
+		
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.A)) {
+			this.Collect(test.GetComponent<Collectible>());
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			this.Drop ();
+		}
+	}
+}
