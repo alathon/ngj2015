932343719b45b9ea2ef4aaa701ccbc6f5b14b2fb
Till Riemer <till.riemer@gmail.com>
2015-02-07 15:04:44 +0100

    added grabbers to prefab, gamecontroller handler

--- Assets/Scripts/GamepadController.cs
+++ Assets/Scripts/GamepadController.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GamepadInput;
 
-public class GamepadController : MonoBehaviour {
+public class GamepadController : MonoBehaviour
+{
+    public bool isLeft;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    private GamePad.Index index; // controller index
+    private GamePad.Axis arm;
+    private GamePad.Axis leg;
+
+    private void Start()
+    {
+        index = isLeft ? GamePad.Index.One : GamePad.Index.Two;
+        arm = isLeft ? GamePad.Axis.LeftStick : GamePad.Axis.RightStick;
+        leg = isLeft ? GamePad.Axis.RightStick : GamePad.Axis.LeftStick;
+    }
+
 	void Update () {
-	    
+	    // arm 2 axes
+	    Vector2 armMovement = GamePad.GetAxis(arm, index);
+
+	    // leg 2 axes
+	    Vector2 legMovement = GamePad.GetAxis(leg, index);
+
+	    // grab/release button
 	}
 }
