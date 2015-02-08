ff5d4a37c8d0ec966672b0c0d84d796edb41a4bb
Till Riemer <till.riemer@gmail.com>
2015-02-08 01:17:55 +0100

    some collectibles in the main scene, TV screens

--- Assets/Scripts/TVController.cs
+++ Assets/Scripts/TVController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TVController : MonoBehaviour {
+public class TVController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+    public AudioClip waltzClip;
+    public AudioClip creepyClip;
+    public AudioClip weirdClip;
+
+    public Texture2D normalChickenTexture;
+    public Texture2D creepyChickenTexture;
+    public Texture2D bakedChickenTexture;
+
+    public Material tvScreenMat;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    audio.clip = waltzClip;
+	    audio.loop = true;
+        audio.Play();
+	    tvScreenMat.mainTexture = normalChickenTexture;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void OnNormal()
+    {
+        audio.clip = waltzClip; // TODO: fade
+        audio.Play();
+        tvScreenMat.mainTexture = normalChickenTexture;
+    }
+
+    public void OnCreepy()
+    {
+        audio.clip = creepyClip; // TODO: fade
+        audio.Play();
+        tvScreenMat.mainTexture = creepyChickenTexture;
+    }
+
+    public void OnWeird()
+    {
+        audio.clip = weirdClip; // TODO: fade
+        audio.Play();
+        tvScreenMat.mainTexture = bakedChickenTexture;
+    }
 }
